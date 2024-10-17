a1194f01df9e0031fc037dcc8e6e4ac512f72e74
hyunwlee <hwlee3928@gmail.com>
2024-10-17 20:15:05 +0900

    feat: Issues component

--- src/app/_components/Issues/index.tsx
+++ src/app/_components/Issues/index.tsx
@@ -1,7 +1,27 @@
 'use client';
 
-import { Section } from '@components/Section';
+import { motion } from 'framer-motion';
+import Link from 'next/link';
+import IssueCard from '@components/IssueCard';
+import { Section } from '@outer_components/layout';
+import { bookkFontMyungjo } from '@styles/font';
+import { type IssueType } from '@types';
 
-export default function Issues() {
-  return <Section heading="Issues">issues</Section>;
+export default function Issues({ issues }: { issues: IssueType[] }) {
+  return (
+    <Section heading="Issues">
+      <motion.ul className="grid grid-cols-1 tablet:grid-cols-2 laptop:grid-cols-3 gap-4">
+        {issues?.map(issue => (
+          <Link
+            key={issue.id}
+            href={`/issues/${issue.number}`}
+            passHref
+            className={`aspect-video ${bookkFontMyungjo.className} font-bold text-2xl`}
+          >
+            <IssueCard issue={issue} />
+          </Link>
+        ))}
+      </motion.ul>
+    </Section>
+  );
 }
