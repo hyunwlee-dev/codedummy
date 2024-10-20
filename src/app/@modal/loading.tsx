11e696a0005bb01f8b5301d726fbca58efecfddd
hyunwlee <hwlee3928@gmail.com>
2024-10-20 13:40:11 +0900

    fix: modal loading position to 'fixed'

--- src/app/@modal/loading.tsx
+++ src/app/@modal/loading.tsx
@@ -2,7 +2,7 @@ import Image from 'next/image';
 
 export default function Loading() {
   return (
-    <div className="absolute top-1/2 left-1/2 z-50 -translate-x-1/2 -translate-y-1/2 w-24 aspect-video">
+    <div className="fixed top-1/2 left-1/2 z-50 -translate-x-1/2 -translate-y-1/2 w-24 aspect-video">
       <Image
         fill
         sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
